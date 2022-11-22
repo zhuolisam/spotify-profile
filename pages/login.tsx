5691acee915bdc82fa5dc858e1290309af32f2ff
zhuolisam <zhuolisam0627@gmail.com>
2022-11-23 02:30:04 +0800

    added fix to redirect url

--- pages/login.tsx
+++ pages/login.tsx
@@ -9,7 +9,7 @@ import axios from 'axios';
 import { AuthContext } from 'providers/AuthContext';
 
 const Login: NextPage = () => {
-  const AUTH_URL = `https://accounts.spotify.com/authorize?response_type=code&client_id=0b655a2196a541f4a00f7467e8496d0a&response_type=code&redirect_uri=http://localhost:3000/login&scope=user-read-private%20user-top-read%20playlist-read-private%20playlist-read-collaborative%20user-read-recently-played%20user-follow-read&state=hello`;
+  const AUTH_URL = `https://accounts.spotify.com/authorize?response_type=code&client_id=0b655a2196a541f4a00f7467e8496d0a&response_type=code&redirect_uri=${process.env.NEXT_PUBLIC_BASE_URL}/login&scope=user-read-private%20user-top-read%20playlist-read-private%20playlist-read-collaborative%20user-read-recently-played%20user-follow-read&state=hello`;
 
   const router = useRouter();
 
